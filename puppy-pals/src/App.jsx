0923251e364ff93dbda6b567a777802154101c4b
Annorinoo <annjenelr@gmail.com>
2023-08-01 15:33:36 -0700

    first commit and adding files needed

---
+++ puppy-pals/src/App.jsx
@@ -0,0 +1,36 @@
+import { puppyList } from './data.js';
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const handlePuppyClick = (id) => {
+    setFeatPupId(id);
+  };
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => (
+        <p onClick={() => handlePuppyClick(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
+      
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ puppy-pals/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Other configurations...
+
+export default defineConfig({
+  plugins: [react()],
+  // Other configurations...
+});
